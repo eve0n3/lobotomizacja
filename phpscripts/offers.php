7fc6df995277b458abd2aef218c28c3786f83b44
eve0n3 <szejmonek@gmail.com>
2026-02-26 16:17:46 +0100

    prawie skończone offers.php

--- phpscripts/offers.php
+++ phpscripts/offers.php
@@ -15,53 +15,67 @@
     $dataJSON = file_get_contents('php://input');
     $data = json_decode( $dataJSON, TRUE ); //convert JSON into array
 
-    $orderCrit = $data["orderCrit"] ?? "utworzenie";//domyślnie "utworzenie"
-    $orderType = $data["orderType"] ?? "DESC";//domyślnie "DESC"
     $miasto = $data["miasto"] ?? null;
     $kategoria = $data["kategoria"] ?? null;
-    $limit = $data["limit"] ?? 20; //domyślnie 20
+    $cena = $data["cena"] ?? null;
 
-    $id = $data["id"] ?? null; //jak id jest wysłane używamy trybu jednego artykułu
+    $page = $data["page"] ?? 1; //page 1 default
+    $limit = $data["limit"] ?? 12; //12 art per page default
 
     echo json_encode([
         "debugvals" => [
-            "orderCrit" => $orderCrit,
-            "orderType" => $orderType,
             "miasto" => $miasto,
             "kategoria" => $kategoria,
+            "cena" => $cena,
             "limit" => $limit,
-            "id" => $id,
+            "page" => $page,
         ]
     ]);
 
 
     include 'dbconnect.php';
 
+    $sqlQueryStart = "SELECT * FROM ogloszenia_oferty";
 
-    $sqlquery = "SELECT * FROM ogloszenia_oferty";
-    if(is_null($id)){//tryb listy ogłoszeń
-        /*
-        TODO: co się dzieje gdy sortujesz od miasto, kategorii i obu
-        czyli multimode
-        */
+    $criteria = [];
 
+    $params = [];
+    $paramTypes = "";
+
+    if(!is_null($miasto) || !is_null($kategoria) || !is_null($cena)){
+        $sqlQueryStart .= " WHERE ";
+
+        if(!is_null($miasto)){
+            array_push($criteria, "miasto = ?");
+            array_push($params, &$miasto);
+            $paramTypes .= "s";
+        }
+        if(!is_null($kategoria)){
+            array_push($criteria, "kategoria = ?");
+            array_push($params, &$miasto);
+            $paramTypes .= "s";
+        }
+        if(!is_null($cena)){
+            array_push($criteria, "cena = ?");
+            array_push($params, &$cena);
+            $paramTypes .= "s";
+        }
+        $sqlQueryStart .= join(" AND ", $criteria);
+        
         echo json_encode([
-            "debug_multimode" => [
-                "sql" => $sqlquery,
-            ]
-        ]);
-    }else{//tryb jednego artykułu
-        $sqlquery .= " WHERE id = ?";
-        $stmt = $conn->prepare($sqlquery);
-        $stmt->bind_param("s", $id);
+            "query" => $sqlQueryStart,
+            "no miasto?" => is_null($miasto),
+            "no kategoria?" => is_null($kategoria),
+            "no cena?" => is_null($cena)
+        ])
+        
+        array_push($params, $limit);
+        $paramTypes .= "s";
+        array_push($params, $offset); // zrob offset
+    }else{
 
-        echo json_encode([
-            "debug_singlemode" => [
-                "id" => $id,
-                "sql" => $sqlquery,
-            ]
-        ]);
     }
+
     
 
 
