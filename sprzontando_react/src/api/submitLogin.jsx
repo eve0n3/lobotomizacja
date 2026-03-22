63e0ab2f93ac64a1a0a487cc2bc53aeda5020a79
eveb <szejmonek@gmail.com>
2026-03-23 00:22:50 +0100

    łojapierdole! cookies robi cookie i cors kocham cors yippie!!!!!!

--- sprzontando_react/src/api/submitLogin.jsx
+++ sprzontando_react/src/api/submitLogin.jsx
@@ -7,6 +7,7 @@ export const submitLogin = async (data) => {
       headers: {
         "Content-Type": "application/json",
       },
+      withCredentials: true,
     });
     console.log(response);
     return { success: true, message: "Logged successfully" };
