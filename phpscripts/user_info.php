ee001e85ee2f5db5103402d1499b7cebb60c9f1b
ew0r <pwa.gry@interia.pl>
2026-04-24 19:10:24 +0200

    add: info o userze po id

---
+++ phpscripts/user_info.php
@@ -0,0 +1,43 @@
+<?php
+    header('Access-Control-Allow-Origin: *'); //domena na produkcji 
+    header('Access-Control-Allow-Methods: POST,OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Credentials: true');
+    header('Content-type: application/json');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { //wazne bez tego bledy nie dzialaja
+    http_response_code(200);
+    exit(0);
+    }
+
+    //dane z react
+    $dataJSON = file_get_contents('php://input');
+    $data = json_decode( $dataJSON, TRUE ); //convert JSON into array
+    $user_id = trim($data['user_id']); 
+    
+
+    //połączenie z bazą danych
+    include 'dbconnect.php';
+
+$stmt = $conn->prepare('SELECT nazwa, email,ostatnie_zlecenie FROM users WHERE id = ?');
+$stmt->bind_param('i', $user_id);
+
+
+if($stmt->execute()){
+    $res = $stmt->get_result();
+    $user = $res->fetch_array(MYSQLI_ASSOC);
+        
+    echo json_encode([
+        "success" => true,
+        "data" => $user
+    ]);
+}else{
+    echo json_encode([
+        "success" => false,
+        "message" => "Prosze spróbować ponownie później"
+    ]);
+    http_response_code(503);
+    }
+
+
+?>
