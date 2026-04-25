d6a2ad8ed2f585f3323452f5019825cb52385635
eveb <szejmonek@gmail.com>
2026-04-25 10:59:10 +0200

    teraz actually zglaszanie działa

---
+++ phpscripts/report.php
@@ -0,0 +1,36 @@
+<?php 
+    header('Access-Control-Allow-Origin: *'); 
+    header('Access-Control-Allow-Methods: POST,OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Content-type: application/json');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { //wazne bez tego bledy nie dzialaja
+    http_response_code(200);
+    exit(0);
+    }
+
+    //dane z react
+    $dataJSON = file_get_contents('php://input');
+    $data = json_decode( $dataJSON, TRUE ); //convert JSON into array
+
+    $id = $data['id'];
+
+    include 'dbconnect.php';
+
+    $sqlquery = "UPDATE ogloszenia_oferty SET report_count = report_count + 1 WHERE id = ?";
+    $stmt = $conn->prepare($sqlquery);
+
+    $stmt->bind_param("i",$id);
+
+    if($stmt->execute()){
+        echo json_encode([
+            "succes" => true,
+            "message" => "Pomyślnie zgłoszono"
+        ]);
+    }else{
+        echo json_encode([
+            "succes" => false,
+            "message" => "Wystąpił błąd, prosze spróbować później"
+        ]);
+        http_response_code(503);
+    }
