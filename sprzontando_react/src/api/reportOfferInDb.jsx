409269e97626e851344c06bc3c3b093047e393ed
cact00s <m.lesniowska07@gmail.com>
2026-04-27 23:07:37 +0200

    giga ZGŁASZANIE

---
+++ sprzontando_react/src/api/reportOfferInDb.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { ADD_OFFER_URL, REPORT_OFFER_URL } from "../../utils/consts";
+
+export const reportOfferInDb = async (id) => {
+  try {
+    const response = await axios.post(
+      REPORT_OFFER_URL,
+      { id: id },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    console.log(response.data.success);
+    if (response.data.success === true) {
+      return { success: true, message: "Pomyślnie zgłoszono ogłoszenie." };
+    } else {
+      throw new Error({ message: "Wystąpił błąd" });
+    }
+  } catch (error) {
+    return {
+      success: false,
+      status: error.response?.data?.status,
+      message: error.response?.data?.message || error.message,
+    };
+  }
+};
