2e42887ea37f2d46bef8365d8b43d30fd56cb147
kastaryndak <kastaryndak@gmail.com>
2026-05-25 20:00:16 +0200

    weryfikacja

--- sprzontando_react/src/sprzontando/registerPage/SuccessVerification.jsx
+++ sprzontando_react/src/sprzontando/registerPage/SuccessVerification.jsx
@@ -1,5 +1,4 @@
 import Container from "@mui/material/Container";
-import CheckCircleOutlineOutlined from "@mui/icons-material/CheckCircleOutlineOutlined";
 import RegisterForm from "./RegisterForm";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -12,12 +11,49 @@ function SuccessVerification() {
     navigate("/login");
   };
   return (
-    <Container>
-      <CheckCircleOutlineOutlined />
-      <Typography>Pomyślnie zweryfikowano !</Typography>
-      <Typography>Kliknij przycisk aby przejść na stronę logowania</Typography>
-      <Button onClick={handleClick}>zaloguj się</Button>
+    <Container 
+    maxWidth={false}
+    sx={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#f5f5f5",
+    }}>
+      <Container
+      maxWidth="sm"
+      sx={{
+        backgroundColor: "white",
+        borderRadius: 3,
+        boxShadow: 3,
+        p: 5,
+      }}
+    >
+      <Typography 
+        variant="h4"
+        sx={{
+          textAlign: "center",
+          mb: 3,
+        }}>Pomyślnie zweryfikowano !</Typography>
+      <Typography
+        sx={{
+          textAlign: "center",
+          mb: 1,
+          fontSize: "1.2rem",
+        }}>Kliknij przycisk aby przejść na stronę logowania</Typography>
+      <Button fullWidth
+        variant="contained"
+        size="large"
+        onClick={handleClick}
+        sx={{
+          py: 1.5,
+          borderRadius: 2,
+          fontWeight: "bold",
+        }}>zaloguj się</Button>
+
+      </Container>
     </Container>
+    
   );
 }
 
